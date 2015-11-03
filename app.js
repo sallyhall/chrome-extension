f8211a89f028f6e079f76ddc69e45b1db5331230
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-03 10:21:22 -0500

    refactored to use object literals

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-
-$(".subReddit").on("submit", function(event){
-  event.preventDefault();
-  var subReddit = $("input").val();
-  $(".pics").html("");
-  $.ajax({
-              url: "https://www.reddit.com/r/"+subReddit+".json",
-              method: "GET",
-              success: function(data) {
-                  var redditsArray = data.data.children;
-                  _.each(redditsArray, function(post) {
-                          $(".pics").append("<div class='animalPic'><a target='_blank' href='http://reddit.com"
-                                            + post.data.permalink
-                                            + "'><img src='"
-                                            + post.data.preview.images[0].source.url
-                                            + "'></a><h2>"+ post.data.title+ "</h2></div>");
-                      })
-                  }
-              });
-
+$("document").ready(function() {
+    picPage.init();
 });
+
+var picPage = {
+    subReddit: "aww",
+    init: function() {
+        picPage.events();
+        picPage.styling();
+    },
+    events: function() {
+        $(".subReddit").on("submit", function() {
+            event.preventDefault();
+            picPage.subReddit = $("input").val();
+            $(".pics").html("");
+            picPage.loadPics();
+        });
+    },
+    styling: function() {
+        picPage.loadPics();
+    },
+    loadPics: function() {
+        $.ajax({
+            url: "https://www.reddit.com/r/" + picPage.subReddit +
+                ".json",
+            method: "GET",
+            success: function(data) {
+                var redditsArray = data.data.children;
+                _.each(redditsArray, function(post) {
+                    $(".pics").append(
+                        "<div class='animalPic'><a target='_blank' href='http://reddit.com"
+                        + post.data.permalink
+                        + "'><img src='"
+                        + post.data.thumbnail
+                        + "'></a><h2>"
+                        + post.data.title
+                        + "</h2></div>");
+                })
+            }
+        });
+    },
+};
